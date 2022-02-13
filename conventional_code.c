53075a45bf8f5882d2d9ed8cb3d520f350ae5be3
Hugo Sobral <hugosobral10@gmail.com>
2022-02-13 20:23:06 -0300

    Create conventional file

--- conventional_code.c
+++ conventional_code.c
@@ -2,7 +2,10 @@
 #include <stdlib.h>
 #include <math.h>
 
-float *generateVector(float *vector, int len) {
+float *generateVector(int len) {
+    float *vector;
+    vector = malloc(len*sizeof(float));
+
     for (int i=0; i<len; i++) {
         vector[i] = pow((i-len/2), 2);
     }
@@ -10,7 +13,7 @@ float *generateVector(float *vector, int len) {
     return vector;
 }
 
-float *refineVector(float *vector, int len) {
+float *refineVector(float vector[], int len) {
     for (int i=0; i<len; i++) {
         vector[i] = sqrt(vector[i]);
     }
@@ -36,4 +39,25 @@ float findGreatest(float *vector, int len) {
     }
 
     return greatest;
+}
+
+int main() {
+    float *v;
+    v = generateVector(10);
+
+    for (int i=0; i<10; i++) printf("%f ", v[i]);
+    printf("\n");
+
+    v = refineVector(v, 10);
+
+    for (int i=0; i<10; i++) printf("%f ", v[i]);
+    printf("\n");
+
+    float smallest = findSmallest(v, 10);
+    float greatest = findGreatest(v, 10);
+
+    printf("Smallest: %f\n", smallest);
+    printf("Greatest: %f\n", greatest);
+
+    return 0;
 }
